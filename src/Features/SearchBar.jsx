a86d58c26f3e5abde12bf0bb808d46b71c7c84d0
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-04-02 03:02:41 +0100

    complete the header middle section

--- src/Features/SearchBar.jsx
+++ src/Features/SearchBar.jsx
@@ -1,17 +1,17 @@
 import { BiSearch } from "react-icons/bi";
 function SearchBar() {
   return (
-    <div className="hidden lg:flex items-center  bg-[#F3F5F9]  rounded-md">
-      <div className="py-3 px-4 flex gap-3  w-[500px] border-r-2">
-        <BiSearch size={28} />
+    <div className="flex items-center bg-[#F3F5F9]  rounded-md text-xs md:text-base">
+      <div className="py-3 px-4 flex gap-3 w-[180px] md:w-[300px] lg:w-[500px]  border-r-2">
+        <BiSearch className="w-5 h-5 md:w-7 md:h-7" />
         <input
           type="text"
           placeholder="Searching for..."
-          className="px-3 outline-none w-full border-l-2 bg-transparent hidden lg:inline-block"
+          className="px-3 outline-none w-full border-l-2 bg-transparent lg:inline-block"
         ></input>
       </div>
 
-      <select className="py-3 px-5 appearance-none bg-transparent cursor-pointer outline-none bg-[url('src/assets/down-arrow.png')] bg-no-repeat top-3 right-3">
+      <select className="py-3 pl-2  md:pl-5 pr-7 md:pr-10 appearance-none bg-transparent cursor-pointer outline-none bg-[url('src/assets/down-arrow.png')] bg-no-repeat bg-[center_right_9px] md:bg-[center_right_14px] ">
         <option value="All Categories">All Categories </option>
         <option value="Car">Car</option>
         <option value="Clothes">Clothes</option>
--- src/ui/HeaderMiddle.jsx
+++ src/ui/HeaderMiddle.jsx
@@ -1,29 +1,37 @@
 import { IoIosMenu } from "react-icons/io";
 import { HiOutlineUser } from "react-icons/hi2";
-import { BiSearch } from "react-icons/bi";
 import { HiOutlineShoppingBag } from "react-icons/hi2";
 import SearchBar from "../Features/SearchBar";
 import { IconContext } from "react-icons";
 
 function HeaderMiddle({ handleOpen }) {
   return (
-    <div className=" text-center w-full bg-white p-3 lg:p-10">
+    <div className=" text-center w-full bg-white p-3 lg:p-5">
       <IconContext.Provider
         value={{ size: "23", color: "#7d879c", className: "cursor-pointer" }}
       >
         <div className="flex justify-center items-center max-w-screen-xl mx-auto relative ">
           <button className="z-1 lg:hidden bg-blue-500" onClick={handleOpen}>
-            <IoIosMenu className="absolute top-0 bottom-0 my-auto left-6" />
+            <IoIosMenu className="absolute top-4  left-6" />
+            {/* <IoIosMenu className="absolute top-0 bottom-0 my-auto left-6" /> */}
           </button>
           {/* <Nav isOpen={isOpen} handleOpen={handleOpen} /> */}
-          <div className=" flex flex-col border-2 justify-center w-11 h-11 lg:w-14 lg:h-14 rounded-full border-[#2b3445] items-center lg:absolute lg:top-0 lg:bottom-0 lg:my-auto lg:left-6">
-            <img src="src/assets/market1.png" alt="market" />
+          <div className="flex flex-col justify-center items-center gap-5">
+            <div className=" flex flex-col  border-2 justify-center w-11 h-11 lg:w-14 lg:h-14 rounded-full border-[#2b3445] items-center lg:absolute lg:top-0 lg:bottom-0 lg:my-auto lg:left-6">
+              <img src="src/assets/market1.png" alt="market" />
+            </div>
+            <SearchBar />
           </div>
-          <SearchBar />
-          <div className="flex justify-center items-center gap-2  absolute top-0 bottom-0 my-auto right-6 ">
-            <BiSearch />
-            <HiOutlineUser oncli />
-            <HiOutlineShoppingBag />
+          <div className="flex justify-center items-center gap-2  absolute top-1 right-6 ">
+            {/* <button className="hover:bg-[#F3F5F9] rounded-full p-2">
+              <BiSearch className="lg:hidden" />
+            </button> */}
+            <button className="hover:bg-[#F3F5F9] rounded-full p-2">
+              <HiOutlineUser />
+            </button>
+            <button className="hover:bg-[#F3F5F9] rounded-full p-2">
+              <HiOutlineShoppingBag />
+            </button>
             <p className="w-5 h-5 bg-[#4BB4B4] rounded-full text-center text-[13px] text-white font-semibold absolute top-2 -right-4">
               3
             </p>
